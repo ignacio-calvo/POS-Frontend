152683514ee7e55c2b0876ee8d3229c4b27ab796
Ignacio Calvo <ignaciocalvo@live.com.ar>
2024-08-24 20:07:37 -0300

    Revert "Moving files to parent path"
    This reverts commit de6a16d19d7459e992932f75e74875df0bc168f1.

--- frontend.client/src/main.tsx
+++ frontend.client/src/main.tsx
@@ -1,3 +1,4 @@
+// File: frontend.client/src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
